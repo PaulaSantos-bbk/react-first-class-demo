a6442fb86ebd715894a541a7b65116fc414682c3
unknown <pangeriz@outlook.com>
2020-05-25 11:06:30 +0100

    Updates

---
+++ precache-manifest.5fb9d3f3e6338939ada53d5c7938647d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f15511b8d15c6f2fe26185d5c87dc4",
+    "url": "/react-first-class-demo/index.html"
+  },
+  {
+    "revision": "a765dd0f9399f9f54559",
+    "url": "/react-first-class-demo/static/css/main.62fa7d98.chunk.css"
+  },
+  {
+    "revision": "f3662d6d0d7776715a37",
+    "url": "/react-first-class-demo/static/js/2.cedb6775.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-first-class-demo/static/js/2.cedb6775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a765dd0f9399f9f54559",
+    "url": "/react-first-class-demo/static/js/main.07ff3003.chunk.js"
+  },
+  {
+    "revision": "f721de9b27af266e73e9",
+    "url": "/react-first-class-demo/static/js/runtime-main.a541b06d.js"
+  }
+]);
